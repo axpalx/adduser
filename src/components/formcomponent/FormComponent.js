031683a98d3bdf2f5302d9c02188cbd8a7aec3b7
Alex Pulido <axpalx@gmail.com>
2020-11-30 20:49:09 -0300

    AddUser v 1.1

--- src/components/formcomponent/FormComponent.js
+++ src/components/formcomponent/FormComponent.js
@@ -1,6 +1,38 @@
 import { useForm } from "../../hooks/useForm";
 import { postUser } from "../request/postUser";
 
+import styled from "styled-components";
+
+const BodyForm = styled.div`
+  width: 400px;
+  height: 35vh;
+  margin: 20vh auto;
+  background: #6c7a89;
+  display: flex;
+  flex-direction: column;
+  padding-top: 15px;
+  border-radius: 5%;
+`;
+
+const FieldInput = styled.div`
+  align-items: right;
+  margin: 10px auto;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ButtonSave = styled.button`
+  width: 80px;
+  height: 35px;
+  border-radius: 5px;
+`;
+
+const InputDate = styled.input`
+  height: 25px;
+  border-radius: 5px;
+`;
+
 const FormComponent = () => {
   const { form, onChange, resetForm } = useForm({
     name: "",
@@ -39,58 +71,60 @@ const FormComponent = () => {
   };
 
   return (
-    <div onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Nome: </label>
-        <input
-          value={form.name}
-          name="name"
-          type="text"
-          id="name"
-          placeholder="Nome"
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="age">Idade: </label>
-        <input
-          value={form.age}
-          name="age"
-          type="number"
-          id="age"
-          placeholder="Idade"
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email">E-mail: </label>
-        <input
-          value={form.email}
-          name="email"
-          type="email"
-          id="email"
-          placeholder="E-mail"
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="city">cidade: </label>
-        <input
-          value={form.city}
-          name="city"
-          type="text"
-          id="city"
-          placeholder="Cidade"
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <button onClick={onClickInsertUser}>Salvar</button>
-      </div>
+    <div>
+      <BodyForm onSubmit={handleSubmit}>
+        <FieldInput>
+          <label htmlFor="name">Nome: </label>
+          <InputDate
+            value={form.name}
+            name="name"
+            type="text"
+            id="name"
+            placeholder="Nome"
+            onChange={handleInputChange}
+            required
+          />
+        </FieldInput>
+        <FieldInput>
+          <label htmlFor="age">Idade: </label>
+          <InputDate
+            value={form.age}
+            name="age"
+            type="number"
+            id="age"
+            placeholder="Idade"
+            onChange={handleInputChange}
+            required
+          />
+        </FieldInput>
+        <FieldInput>
+          <label htmlFor="email">E-mail: </label>
+          <InputDate
+            value={form.email}
+            name="email"
+            type="email"
+            id="email"
+            placeholder="E-mail"
+            onChange={handleInputChange}
+            required
+          />
+        </FieldInput>
+        <FieldInput>
+          <label htmlFor="city">cidade: </label>
+          <InputDate
+            value={form.city}
+            name="city"
+            type="text"
+            id="city"
+            placeholder="Cidade"
+            onChange={handleInputChange}
+            required
+          />
+        </FieldInput>
+        <FieldInput>
+          <ButtonSave onClick={onClickInsertUser}>Salvar</ButtonSave>
+        </FieldInput>
+      </BodyForm>
     </div>
   );
 };
